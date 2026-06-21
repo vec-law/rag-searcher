856468c81eca231eebd4332602928f60bb9ed79b
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-06-21 08:18:05 +0200

    feat: add app settings to config

--- rag_searcher/config.py
+++ rag_searcher/config.py
@@ -3,15 +3,29 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    db_name: str
     db_user: str
     db_password: SecretStr
     db_host: str
     db_port: int
-    db_name: str
+
+    page_url: str
+    page_type: str
+    page_max: int
+    fetcher_name: str
+
+    link_expiry_days: int
+
     embedding_model_name: str
     embedding_vector_size: int
-    openai_api_key: SecretStr | None = None
+
+    semantic_search_limit: int
+
+    llm_name: str
+    rag_limit: int
+
     hf_token: SecretStr | None = None
+    openai_api_key: SecretStr | None = None
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
