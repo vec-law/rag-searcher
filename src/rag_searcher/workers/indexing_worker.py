bf5dcbf095c1fc7a8de3b24e70e4f13d618f67f4
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-06-29 06:03:23 +0200

    feat: add indexing loop skeleton

--- src/rag_searcher/workers/indexing_worker.py
+++ src/rag_searcher/workers/indexing_worker.py
@@ -1,3 +1,6 @@
+import time
+import traceback
+
 from rag_searcher.db.pool import pool
 from rag_searcher.services.indexing.page import get_page, delete_expired_page_links, set_page_contents_pending
 
@@ -6,10 +9,17 @@ def main():
     pool.open()
     try:
         page = get_page()
-        delete_expired_page_links(page.id)
-        set_page_contents_pending(page.id)
 
-        # TODO: pętla indeksacyjna
+        while True:
+            try:
+                delete_expired_page_links(page.id)
+                set_page_contents_pending(page.id)
+            except Exception:
+                traceback.print_exc()
+                time.sleep(60)
+                continue
+
+            time.sleep(3600)
 
     finally:
         pool.close()
