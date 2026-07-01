a00ae9c74ae2ac3a440c8902767cf80531236a55
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-07-01 02:57:30 +0200

    feat: wire get_compute_embedding into indexing_worker

--- src/rag_searcher/workers/indexing_worker.py
+++ src/rag_searcher/workers/indexing_worker.py
@@ -3,11 +3,13 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 
 from rag_searcher.db.pool import pool
+from rag_searcher.services.page_setup import setup_page
 from rag_searcher.services.indexing.page import (
-    setup_page, delete_expired_page_links, fetch_page_links,
+    delete_expired_page_links, fetch_page_links,
     set_page_contents_pending, get_page_content_ids,
 )
 from rag_searcher.services.indexing.content import fetch_content
+from rag_searcher.services.embedder import get_compute_embedding
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +19,7 @@ def main():
     pool.open()
     try:
         page_id = setup_page()
+        compute_embedding = get_compute_embedding(page_id)
 
         while True:
             try:
