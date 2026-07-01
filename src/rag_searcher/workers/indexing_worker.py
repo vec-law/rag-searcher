e497eaa5f5435445c74b5aedb0f56c4470a3ece0
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-07-01 09:00:11 +0200

    feat: wire embedding computation into indexing_worker

--- src/rag_searcher/workers/indexing_worker.py
+++ src/rag_searcher/workers/indexing_worker.py
@@ -7,9 +7,11 @@ from rag_searcher.services.page_setup import setup_page
 from rag_searcher.services.indexing.page import (
     delete_expired_page_links, fetch_page_links,
     set_page_contents_pending, get_page_content_ids,
+    set_page_embeddings_pending, get_page_embedding_ids
 )
 from rag_searcher.services.indexing.content import fetch_content
-from rag_searcher.services.embedder import get_compute_embedding
+from rag_searcher.services.indexing.embedding import compute_embedding
+from rag_searcher.services.embedder import get_embed
 
 logger = logging.getLogger(__name__)
 
@@ -19,17 +21,23 @@ def main():
     pool.open()
     try:
         page_id = setup_page()
-        compute_embedding = get_compute_embedding(page_id)
+        embed = get_embed(page_id)
 
         while True:
             try:
                 delete_expired_page_links(page_id)
                 fetch_page_links(page_id)
-                set_page_contents_pending(page_id)
 
+                set_page_contents_pending(page_id)
                 content_ids = get_page_content_ids(page_id)
                 with ThreadPoolExecutor(max_workers=2) as executor:
                     executor.map(fetch_content, content_ids)
+
+                set_page_embeddings_pending(page_id)
+                embedding_ids = get_page_embedding_ids(page_id)
+                with ThreadPoolExecutor(max_workers=5) as executor:
+                    for embedding_id in embedding_ids:
+                        executor.submit(compute_embedding, embed, embedding_id)
 
             except Exception:
                 logger.exception("Błąd w pętli indeksującej")
