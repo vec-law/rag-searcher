8c5c65fa34e576ae1bc6dba10cfd7bfbc45ed142
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-07-01 08:59:22 +0200

    refactor: rename get_compute_embedding to get_embed

--- src/rag_searcher/services/embedder.py
+++ src/rag_searcher/services/embedder.py
@@ -1,13 +1,13 @@
 from rag_searcher.core.embedder import Embedder
 from rag_searcher.config import settings
-from rag_searcher.db.queries.page import get_page_embedding_config
+from rag_searcher.db.queries.page import get_page_embedding_config  # TODO: potwierdź nazwę
 
 
-def get_compute_embedding(page_id: int):
+def get_embed(page_id: int):
     model_name, _ = get_page_embedding_config(page_id)
     embedder = Embedder(
         model_name,
         openai_api_key=settings.openai_api_key.get_secret_value(),
         hf_token=settings.hf_token.get_secret_value() if settings.hf_token else None,
     )
-    return embedder.compute_embedding
+    return embedder.embed
